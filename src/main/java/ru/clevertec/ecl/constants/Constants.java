c9e36f8d1621369556bcd67a0890881317d83332
mechigurov <vip.moneta95@mail.ru>
2022-04-20 00:36:49 +0700

    added a new constants

--- src/main/java/ru/clevertec/ecl/constants/Constants.java
+++ src/main/java/ru/clevertec/ecl/constants/Constants.java
@@ -1,9 +1,11 @@
 package ru.clevertec.ecl.constants;
 
+import java.util.Locale;
+
 public class Constants {
 
-    public static final String NOT_FOUND_MSG = "tag not found";
-    public static final String DATABASE_IS_EMPTY_MSG = "database is empty";
-    public static final String NOT_REMOVED_MSG = "not removed";
+    public static final String EXCEPTION_MESSAGE_ENTITY_NOT_FOUND_FORMAT = "Entity with id: %s is not exist";
+    public static final String LOG_EXCEPTION_MESSAGE_FORMAT = "Exception in class: {} --- [{}]";
+    public static final Locale LOCALE = new Locale("ru");
 
 }

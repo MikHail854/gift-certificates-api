3082bb98a3f76c260227d6ca413b44ae894c0f79
mechigurov <vip.moneta95@mail.ru>
2022-04-28 18:45:00 +0700

    added tables user and order

--- src/main/resources/db/migration/V1.1__schema.sql
+++ src/main/resources/db/migration/V1.1__schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE gift_certificate_tag
 
 CREATE TABLE gift_certificate
 (
-    id               SERIAL NOT NULL,
+    id               SERIAL UNIQUE NOT NULL,
     name             VARCHAR(25),
     description      VARCHAR(100),
     price            NUMERIC(10, 2),
@@ -18,7 +18,25 @@ CREATE TABLE gift_certificate
 
 CREATE TABLE tag
 (
-    id   SERIAL NOT NULL,
+    id   SERIAL UNIQUE NOT NULL,
     name VARCHAR(25) UNIQUE,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE user_data
+(
+    id         SERIAL UNIQUE NOT NULL,
+    first_name VARCHAR(30)   NOT NULL,
+    last_name  VARCHAR(30)   NOT NULL
+);
+
+CREATE TABLE order_data
+(
+    id             SERIAL UNIQUE  NOT NULL,
+    user_id        INTEGER        NOT NULL,
+    certificate_id INTEGER        NOT NULL,
+    price          NUMERIC(10, 2) NOT NULL,
+    purchase_date  TIMESTAMP WITHOUT TIME ZONE,
+    FOREIGN KEY (user_id) references user_data (id),
+    FOREIGN KEY (certificate_id) references gift_certificate (id)
 );

2e65f1d8f1076f2bac78e9d09002adc19088dc31
mechigurov <vip.moneta95@mail.ru>
2022-05-04 18:19:51 +0700

    save method changed

--- src/main/java/ru/clevertec/ecl/service/impl/TagServiceImpl.java
+++ src/main/java/ru/clevertec/ecl/service/impl/TagServiceImpl.java
@@ -2,6 +2,7 @@ package ru.clevertec.ecl.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.clevertec.ecl.dao.TagRepository;
@@ -11,6 +12,7 @@ import ru.clevertec.ecl.mapper.TagMapper;
 import ru.clevertec.ecl.service.TagService;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -28,13 +30,13 @@ public class TagServiceImpl implements TagService {
     private final TagMapper tagMapper;
 
     @Override
-    public List<TagDTO> findAll() {
-        return tagRepository.findAll().stream().map(tagMapper::tagToTagDTO).collect(Collectors.toList());
+    public List<TagDTO> findAll(Pageable pageable) {
+        return tagRepository.findAll(pageable).stream().map(tagMapper::toTagDTO).collect(Collectors.toList());
     }
 
     @Override
     public TagDTO findById(int id) {
-        final TagDTO dto = tagRepository.findById(id).map(tagMapper::tagToTagDTO)
+        final TagDTO dto = tagRepository.findById(id).map(tagMapper::toTagDTO)
                 .orElseThrow(() -> new EntityNotFoundException(String.format(EXCEPTION_MESSAGE_ENTITY_NOT_FOUND_FORMAT, "tag", id)));
         log.info("found tag - {}", dto);
         return dto;
@@ -45,10 +47,16 @@ public class TagServiceImpl implements TagService {
     public TagDTO save(Tag tag) {
         log.info("tag to save to database - {}", tag);
         if (Objects.nonNull(tag.getId()) && tagRepository.findById(tag.getId()).isPresent()) {
-            final Tag tagFromDB = tagMapper.tagDTOToTag(findById(tag.getId()));
-            tag.getGiftCertificates().addAll(tagFromDB.getGiftCertificates());
+            final Tag tagFromDB = tagMapper.toTag(findById(tag.getId()));
+            if (Objects.nonNull(tag.getGiftCertificates())) {
+                tag.getGiftCertificates().addAll(tagFromDB.getGiftCertificates());
+            } else {
+                tag.setGiftCertificates(new ArrayList<>());
+                log.info("!!! tag: {}", tag);
+                tag.getGiftCertificates().addAll(tagFromDB.getGiftCertificates());
+            }
         }
-        final TagDTO saved = tagMapper.tagToTagDTO(tagRepository.save(tag));
+        final TagDTO saved = tagMapper.toTagDTO(tagRepository.save(tag));
         log.info("successful saving of the tag in the database - {}", saved);
         return saved;
     }
@@ -59,7 +67,7 @@ public class TagServiceImpl implements TagService {
         log.info("tag for updating in the database - {}", tagDTO);
         final TagDTO updated = tagRepository.findById(id)
                 .map(tag -> updateTagFromTagDTO(tag, tagDTO))
-                .map(tagMapper::tagToTagDTO)
+                .map(tagMapper::toTagDTO)
                 .orElseThrow(() -> new EntityNotFoundException(String.format(EXCEPTION_MESSAGE_ENTITY_NOT_FOUND_FORMAT, "tag", id)));
         log.info("successful update of the tag in the database - {}", updated);
         return updated;
@@ -76,7 +84,7 @@ public class TagServiceImpl implements TagService {
     @Override
     public TagDTO findTheMostWidelyUsedTag() {
         final TagDTO dto = tagRepository.findTheMostWidelyUsedTag()
-                .map(tagMapper::tagToTagDTO)
+                .map(tagMapper::toTagDTO)
                 .orElseThrow(() -> new EntityNotFoundException(EXCEPTION_MESSAGE_SOMETHING_WENT_WRONG));
         log.info("found the most widely used tag - {}", dto);
         return dto;

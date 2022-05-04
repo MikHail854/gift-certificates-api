8e2d8b821f3b1e34a9b48735e85540c53df4e411
mechigurov <vip.moneta95@mail.ru>
2022-05-04 17:49:12 +0700

    added pageable

--- src/main/java/ru/clevertec/ecl/service/TagService.java
+++ src/main/java/ru/clevertec/ecl/service/TagService.java
@@ -2,12 +2,14 @@ package ru.clevertec.ecl.service;
 
 import ru.clevertec.ecl.dto.TagDTO;
 import ru.clevertec.ecl.entty.Tag;
+import org.springframework.data.domain.Pageable;
+
 
 import java.util.List;
 
 public interface TagService {
 
-    List<TagDTO> findAll();
+    List<TagDTO> findAll(Pageable pageable);
 
     TagDTO findById(int id);
 

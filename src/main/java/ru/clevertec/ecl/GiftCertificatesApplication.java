586dffc60e6da615c843f64143eb971201941cf7
mechigurov <vip.moneta95@mail.ru>
2022-05-17 23:44:19 +0700

    added annotation EnableScheduling

--- src/main/java/ru/clevertec/ecl/GiftCertificatesApplication.java
+++ src/main/java/ru/clevertec/ecl/GiftCertificatesApplication.java
@@ -2,8 +2,14 @@ package ru.clevertec.ecl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+//@EnableFeignClients
 @SpringBootApplication
+//@ServletComponentScan(value = "ru.clevertec.ecl.filter")
 public class GiftCertificatesApplication {
 
     public static void main(String[] args) {

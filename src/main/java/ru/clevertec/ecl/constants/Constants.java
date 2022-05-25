5b1a0dd9b9a501e3711ebfdc3bdd779db3f99f76
mechigurov <vip.moneta95@mail.ru>
2022-05-25 22:07:52 +0700

    removed unused constants

--- src/main/java/ru/clevertec/ecl/constants/Constants.java
+++ src/main/java/ru/clevertec/ecl/constants/Constants.java
@@ -13,18 +13,6 @@ public class Constants {
     public static final int ERROR_CODE_METHOD_ARGUMENT_NOT_VALID_EXCEPTION = 40003;
     public static final int ERROR_CODE_DATA_INTEGRITY_VIOLATION_EXCEPTION = 40004;
 
-    public static final String URL_LOCALHOST = "http://localhost:";
-//    public static final String PATH_FIND_BY_ID = "findById";
-//    public static final String ORDERS = "/orders/";
-
-    public static final String URL_ORDERS_FIND_BY_ID = "http://localhost:%s/orders/%s";
-    public static final String URL_ORDERS_FIND_ORDER_BY_ID_AND_USER_ID = "http://localhost:%s/orders/find?user_id=%s&order_id=%s";
-    public static final String URL_ORDERS_FIND_ORDERS_BY_USER_ID = "http://localhost:%s/orders?user_id=%s";
-
-
-    public static final String URL_ORDERS_GET_SEQUENCE = "http://localhost:%s/sequence";
-
-
-
+    public static final String URL_SEQUENCE_ORDERS = "http://localhost:%s/sequence/orders";
 
 }

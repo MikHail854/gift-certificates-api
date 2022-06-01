a8d240108647814400cfc0ef95a3009cf22eb562
mechigurov <vip.moneta95@mail.ru>
2022-06-02 00:02:05 +0700

    added color statuses of node viability

--- src/main/java/ru/clevertec/ecl/service/impl/HealthCheckServiceImpl.java
+++ src/main/java/ru/clevertec/ecl/service/impl/HealthCheckServiceImpl.java
@@ -6,9 +6,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import ru.clevertec.ecl.config.ServerProperties;
+import ru.clevertec.ecl.config.Cluster;
 import ru.clevertec.ecl.service.HealthCheckService;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 
@@ -17,24 +19,46 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class HealthCheckServiceImpl implements HealthCheckService {
 
+    private final Cluster cluster;
     private final RestTemplate restTemplate;
-    private final ServerProperties serverProperties;
 
     @Override
     @Scheduled(cron = "0 * * * * ?")
     public void nodeHealthCheck() {
-        for (int i = 0; i < serverProperties.getPorts().size(); i++) {
-            try {
-                String url = "http://localhost:" + serverProperties.getPorts().get(i) + "/health-check";
-                final HttpStatus httpStatus = restTemplate.getForObject(url, HttpStatus.class);
-                if (Objects.equals(httpStatus, HttpStatus.OK)) {
-                    log.info("node on port: {} is running", serverProperties.getPorts().get(i));
-                } else {
-                    log.error("node on port: {} is down. HttpStatus: {}", serverProperties.getPorts().get(i), httpStatus);
+        final Map<Integer, Cluster.Node> nodes = cluster.getNodes();
+        for (int i = 0; i < nodes.size(); i++) {
+            final List<Cluster.Replica> replicas = nodes.get(i).getReplicas();
+            for (Cluster.Replica replica : replicas) {
+                try {
+                    String url = "http://localhost:" + replica.getPort() + "/health-check";
+                    final HttpStatus httpStatus = restTemplate.getForObject(url, HttpStatus.class);
+                    if (Objects.equals(httpStatus, HttpStatus.OK)) {
+                        if (!replica.getIsAlive()) {
+                            replica.setIsAlive(true);
+                        }
+                    }
+                } catch (Exception e) {
+                    replica.setIsAlive(false);
                 }
-            } catch (Exception e) {
-                log.error("node on port: {} is down. {}", serverProperties.getPorts().get(i), String.valueOf(e));
             }
+            printStatusNode(nodes, i);
+        }
+    }
+
+    private void printStatusNode(Map<Integer, Cluster.Node> nodes, int i) {
+        final int countOfLiveReplicas = (int) nodes.get(i).getReplicas().stream().filter(Cluster.Replica::getIsAlive).count();
+        switch (countOfLiveReplicas) {
+            case 3:
+                log.info("node {} - green", i);
+                break;
+            case 2:
+                log.warn("node {} - yellow", i);
+                break;
+            case 1:
+                log.warn("node {} - red", i);
+                break;
+            case 0:
+                log.error("node {} has no live replicas", i);
         }
     }
 

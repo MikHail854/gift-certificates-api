a9fb11adf8a36a75d78fbc5a47bcede08c016ab5
mechigurov <vip.moneta95@mail.ru>
2022-07-01 16:44:04 +0700

    added annotation @NoArgsConstructor and @AllArgsConstructor

--- src/main/java/ru/clevertec/ecl/dto/CommitLogDTO.java
+++ src/main/java/ru/clevertec/ecl/dto/CommitLogDTO.java
@@ -1,15 +1,20 @@
 package ru.clevertec.ecl.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpMethod;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommitLogDTO {
     private Integer id;
     private String url;
     private String body;
     private HttpMethod method;
     private TypeObject typeObject;
+    private String portInitiatorLog;
 }

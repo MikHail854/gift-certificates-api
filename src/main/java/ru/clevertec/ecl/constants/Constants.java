2d3a72eb0c9cf80ab9d8c059874df8c0e3935687
mechigurov <vip.moneta95@mail.ru>
2022-07-01 16:36:12 +0700

    added new constants

--- src/main/java/ru/clevertec/ecl/constants/Constants.java
+++ src/main/java/ru/clevertec/ecl/constants/Constants.java
@@ -17,7 +17,17 @@ public class Constants {
 
     public static final String URL_COMMIT_LOG = "http://localhost:%s/commit_log?request_from_client=false";
 
-    public static final String URL_SAVE_GIFT_CERTIFICATE = "http://localhost:%s/gifts/save?save_to_commit_log=false";
+    public static final String URL_CREATE_GIFT_CERTIFICATE = "http://localhost:%s/gifts/create?save_to_commit_log=false";
+    public static final String URL_UPDATE_GIFT_CERTIFICATE = "http://localhost:%s/gifts/update/%s?save_to_commit_log=false";
+    public static final String URL_UPDATE_PRICE_GIFT_CERTIFICATE = "http://localhost:%s/gifts/update/%s/price?save_to_commit_log=false";
+    public static final String URL_UPDATE_DURATION_GIFT_CERTIFICATE = "http://localhost:%s/gifts/update/%s/duration?save_to_commit_log=false";
+    public static final String URL_DELETE_GIFT_CERTIFICATE = "http://localhost:%s/gifts/delete/%s?save_to_commit_log=false";
 
+    public static final String URL_CREATE_TAG = "http://localhost:%s/tags/create?save_to_commit_log=false";
+    public static final String URL_UPDATE_TAG = "http://localhost:%s/tags/update/%s?save_to_commit_log=false";
+    public static final String URL_DELETE_TAG = "http://localhost:%s/tags/delete/%s?save_to_commit_log=false";
+
+    public static final String URL_CREATE_USER = "http://localhost:%s/users/create?save_to_commit_log=false";
+    public static final String URL_DELETE_USER = "http://localhost:%s/users/delete/%s?save_to_commit_log=false";
 
 }

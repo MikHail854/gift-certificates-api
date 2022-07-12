0b61de561b4ee7a7c0b166f9a09631fc2e29eb90
mechigurov <vip.moneta95@mail.ru>
2022-07-12 15:28:44 +0700

    added SMS sending when placing an order

---
+++ src/main/java/ru/clevertec/ecl/service/impl/SmsSenderImpl.java
@@ -0,0 +1,53 @@
+package ru.clevertec.ecl.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import ru.clevertec.ecl.service.SmsSender;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.Base64;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SmsSenderImpl implements SmsSender {
+
+    @Value("${sms.name}")
+    private final String name;
+
+    @Value("${sms.password}")
+    private final String password;
+
+    @Value("${sms.host}")
+    private final String host;
+
+    @Override
+    @SneakyThrows
+    public void sendSms(String phone, String text, String sender) {
+        String authString = name + ":" + password;
+        String authStringEnc = Base64.getEncoder().encodeToString(authString.getBytes());
+
+        URL url = new URL("http", host, 80, "/messages/v2/send/?phone=%2B" + phone + "&text=" + URLEncoder.encode(text, "UTF-8") + "&sender=" + sender);
+        URLConnection urlConnection = url.openConnection();
+        urlConnection.setRequestProperty("Authorization", authStringEnc);
+        InputStream is = urlConnection.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+
+        int numCharsRead;
+        char[] charArray = new char[1024];
+        StringBuffer sb = new StringBuffer();
+        while ((numCharsRead = isr.read(charArray)) > 0) {
+            sb.append(charArray, 0, numCharsRead);
+        }
+        String result = sb.toString();
+        log.info("sms result: {}", result);
+    }
+
+}
